630b0703d0d16c53c3512508ba3c7338b4621bb1
Pranay22507 <pranayk@cybage.com>
2021-02-11 15:01:54 +0530

    first commit

---
+++ POC/config/webpack.config.common.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const CleanWebpackPlugin   = require('clean-webpack-plugin');
+const HtmlWebpackPlugin    = require('html-webpack-plugin');
+
+const helpers              = require('./helpers');
+const isDev                = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    entry: {
+        vendor: './src/vendor.ts',
+        polyfills: './src/polyfills.ts',
+        main: isDev ? './src/main.ts' : './src/main.aot.ts'
+    },
+
+    resolve: {
+        extensions: ['.ts', '.js', '.scss']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.css$/,
+                loaders: 'style-loader!css-loader'
+              },
+            //   {
+            //     test: /\.ts$/,
+            //     // exclude: /node_modules/,
+            //     use: ["awesome-typescript-loader"],
+            //     // include: helpers.root('src', 'seller')
+            //   },
+            // {
+            //     test: /\.ts$/,
+            //     loader: 'ts-loader',
+            //     // exclude: [ /node_modules/ ]
+            //     // include: helpers.root('src', 'app/seller')
+            //   },
+           
+            {
+                test: /\.(scss|sass)$/,
+                use: [
+                    'to-string-loader',
+                    { loader: 'css-loader', options: { sourceMap: isDev } },
+                    { loader: 'sass-loader', options: { sourceMap: isDev } }
+                ],
+                include: helpers.root('src', 'app')
+            }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(
+            helpers.root('dist'), { root: helpers.root(), verbose: true }),
+
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+};
